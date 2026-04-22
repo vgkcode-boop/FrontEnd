24c8e34fa711e2a9093b89a9ea0d35a66ee9584f
Victor Augusto <victorservo@icloud.com>
2026-04-22 02:21:21 -0300

    primeiro commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,18 +1,16 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { App } from './app';
+import { Item } from './item/item';
+import { Produto } from './produto/produto';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Item, Produto],
+  imports: [BrowserModule, HttpClientModule],
+
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,55 @@
 import { Component, signal } from '@angular/core';
+import { Tarefa } from './tarefa';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('TODOapp');
+
+  arrayDeTarefas = signal<Tarefa[]>([]);
+  apiURL: string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    const novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe((resultado) => {
+      console.log(resultado);
+      this.READ_tarefas();
+    });
+  }
+
+  READ_tarefas() {
+    this.http
+      .get<Tarefa[]>(`${this.apiURL}/api/getAll`)
+      .subscribe((resultado) => this.arrayDeTarefas.set(resultado));
+  }
+
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserRemovida);
+    var id = this.arrayDeTarefas()[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe((resultado) => {
+      console.log(resultado);
+      this.READ_tarefas();
+    });
+  }
+
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas().indexOf(tarefaAserModificada);
+    var id = this.arrayDeTarefas()[indice]._id;
+    this.http
+      .patch<Tarefa>(`${this.apiURL}/api/update/${id}`, tarefaAserModificada)
+      .subscribe((resultado) => {
+        console.log(resultado);
+        this.READ_tarefas();
+      });
+  }
 }
